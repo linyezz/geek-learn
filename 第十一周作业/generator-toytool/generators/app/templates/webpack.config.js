c56beea3dd28c8fa2bf56c6085eec50cd5d319d4
极易-张政 <z13816565384@163.com>
2021-11-22 14:12:36 +0800

    十一周作业

---
+++ 第十一周作业/generator-toytool/generators/app/templates/webpack.config.js
@@ -0,0 +1,34 @@
+2 lines (31 sloc)  681 Bytes
+   
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {},
+  module: {
+    rules: [
+      { test: /\.vue$/, use: "vue-loader" },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: "src/*.html", to: "[name].[ext]" }],
+    }),
+  ],
+};
